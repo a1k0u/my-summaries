095ba243545ae12762b102df1045889893e73e93
a1k0u <alko7enko@gmail.com>
2022-08-05 20:36:32 +0300

    Python: Flask: upgrade: 'create new endpoint which shows all post with exact tag; show tags on page content'

--- Python/flask-blog/posts/blueprint.py
+++ Python/flask-blog/posts/blueprint.py
@@ -2,6 +2,7 @@ from flask import Blueprint
 from flask import render_template
 from flask import redirect
 from models import Post
+from models import Tag
 
 posts = Blueprint("posts", __name__, template_folder="templates")
 
@@ -9,10 +10,30 @@ posts = Blueprint("posts", __name__, template_folder="templates")
 @posts.route("/")
 def index():
     __posts = Post.query.all()
-    return render_template("posts/index.html", posts=__posts)
+    webpage_title = "Index page"
+    page_title = "Posts"
+    return render_template(
+        "posts/posts.html",
+        webpage_title=webpage_title,
+        page_title=page_title,
+        posts=__posts,
+    )
 
 
 @posts.route("/<url>")
 def post_content(url: str):
-    post = Post.query.where(Post.title == url).first()
-    return render_template("posts/post_content.html", post=post)
+    post = Post.query.where(Post.url == url).first()
+    return render_template("posts/post_content.html", post=post, tags=post.tags)
+
+
+@posts.route("/tag/<url>")
+def tag_content(url: str):
+    tag = Tag.query.filter(Tag.url == url).first()
+    webpage_title = tag.name
+    page_title = f"Posts for '{tag.name}' tag"
+    return render_template(
+        "posts/posts.html",
+        webpage_title=webpage_title,
+        page_title=page_title,
+        posts=tag.posts,
+    )
