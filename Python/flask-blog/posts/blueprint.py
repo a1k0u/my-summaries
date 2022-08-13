46980b651388c7c36be5b83cf6e9e70c1f5cb715
a1k0u <alko7enko@gmail.com>
2022-08-13 19:45:25 +0300

    Python: Flask: upgrade: 'add ability to search post by some word in nav bar'

--- Python/flask-blog/posts/blueprint.py
+++ Python/flask-blog/posts/blueprint.py
@@ -1,36 +1,44 @@
 from flask import Blueprint
 from flask import render_template
 from flask import redirect
+from flask import request
 from models import Post
 from models import Tag
 
 posts = Blueprint("posts", __name__, template_folder="templates")
 
 
-@posts.route("/")
+@posts.route("/", methods=["GET"])
 def index():
-    __posts = Post.query.all()
+    q = request.args.get("q")
+    list_of_posts = (
+        Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+        if q
+        else Post.query.all()
+    )
+    page_title = "Posts" if not q else f'Posts that contain "{q}"'
     webpage_title = "Index page"
-    page_title = "Posts"
+
     return render_template(
         "posts/posts.html",
         webpage_title=webpage_title,
         page_title=page_title,
-        posts=__posts,
+        posts=list_of_posts,
     )
 
 
-@posts.route("/<url>")
+@posts.route("/<url>", methods=["GET"])
 def post_content(url: str):
     post = Post.query.where(Post.url == url).first()
     return render_template("posts/post_content.html", post=post, tags=post.tags)
 
 
-@posts.route("/tag/<url>")
+@posts.route("/tag/<url>", methods=["GET"])
 def tag_content(url: str):
     tag = Tag.query.filter(Tag.url == url).first()
-    webpage_title = tag.name
     page_title = f"Posts for '{tag.name}' tag"
+    webpage_title = tag.name
+
     return render_template(
         "posts/posts.html",
         webpage_title=webpage_title,
